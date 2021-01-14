bce251fa6653f5726c29cfc78b933550a08da63d
DF1229 <daanfaber1@gmail.com>
2021-01-14 16:46:28 +0100

    Automatically generate and destroy config files
    Config files will be automatically made for new servers, and destroyed for servers the bot has left.

--- custom_modules/serverRegistrar.js
+++ custom_modules/serverRegistrar.js
@@ -1,13 +1,14 @@
-const fs = require('fs');
 const { defaultPrefix } = require('../config.json');
+const fs = require('fs');
 
 module.exports = {
     /**
      * Generates a list of all servers the bot is a member of, and creates the directories for their config files.
+     * Only meant to run once, during the bot's ready event.
      * @param {Client} client 
      * @returns {Object} Amount of new, and old servers found.
      */
-    run(client) {        
+    boot(client) {
         let servers = {};
         var newServers = 0, oldServers = 0;
         client.guilds.cache.forEach(guild => {
@@ -40,5 +41,46 @@ module.exports = {
             servers.oldServers = oldServers;
         });
         return servers;
+    },
+    /**
+     * This method generates default config files for new servers. 
+     * Meant to be executed a limitless amount of times during runtime.
+     * @param {Guild} guild Guild for which default config files should be made
+     * @returns {Boolean} True on success, throws an error on failure.
+     */
+    add(guild) {
+        fs.mkdirSync(`server/${guild.id}`);
+
+        let settings = new Object();
+        settings.name = guild.name;
+        settings.id = guild.id;
+        settings.prefix = defaultPrefix;
+        settings.quotes = false;
+        let settingsStr = JSON.stringify(settings);
+
+        let quotes = new Object();
+        quotes.guildID = guild.id;
+        temp2.quotes = [];
+        let quotesStr = JSON.stringify(quotes);
+
+        fs.writeFileSync(`servers/${guild.id}/settings.json`, settingsStr, 'utf8', (err) => {
+            if (err) throw err;
+        });
+        fs.writeFileSync(`servers/${guild.id}/quotes.json`, quotesStr, 'utf8', (err) => {
+            if (err) throw err;
+        });
+
+        return true;
+    },
+    /**
+     * Recursively deletes any files in regard to the provided guild object.
+     * Meant to be run a limitless amount of times during runtime.
+     * @param {Guild} guild 
+     */
+    remove(guild) {
+        if (!fs.existsSync(`servers/${guild.id}`)) return false;
+
+        fs.rmdirSync(`servers/${guild.id}`, {recursive: true, maxRetries: 0});
+        return true;
     }
 }
