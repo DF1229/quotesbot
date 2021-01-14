04a2797930a69f2b6db739b6a4c17ef16d9a05d8
DF1229 <daanfaber1@gmail.com>
2021-01-14 14:57:32 +0100

    Update logger.js

--- custom_modules/logger.js
+++ custom_modules/logger.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
-module.exports = function log(authorTag, logMsg) {
-    const date = new Date();
-    const UTCstring = date.toISOString().substr(0, 10);
-    const timeString = date.toISOString().substr(11, 8);
+module.exports = {
+    Log(authorTag, logMsg) {
+        const date = new Date();
+        const UTCstring = date.toISOString().substr(0, 10);
+        const timeString = date.toISOString().substr(11, 8);
 
-    var nextEntry = `${timeString} - ${authorTag} ${description}\n`;
+        var nextEntry = `${timeString} - ${authorTag} ${description}\n`;
 
-    fs.appendFile(`./logs/${UTCstring}.txt`, nextEntry, 'utf8', (err) => {
-        if (err) throw err;
-    });
-    console.log(nextEntry);
-}
+        fs.appendFile(`./logs/${UTCstring}.txt`, nextEntry, 'utf8', (err) => {
+            if (err) throw err;
+        });
+        console.log(nextEntry);
+    }
+} 
