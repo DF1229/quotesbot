f2768c35f96a8c28770f9ed9379616f950af0709
DF1229 <daanfaber1@gmail.com>
2021-01-18 13:51:09 +0100

    Added snipe command

---
+++ commands/snipe.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'snipe',
+    description: 'Snipe that deleted message back into existance!',
+    guildOnly: true,
+    args: false,
+    execute(msg) {
+        const dMsg = msg.guild.me.client.deletedMessages.get(msg.channel.id);
+        if (!dMsg) return msg.channel.send(`❌ Couldn't find anything to snipe!`);
+
+        let embed = new Discord.MessageEmbed()
+            .setAuthor(dMsg.author.tag, dMsg.author.avatarURL({ dynamic: true}))
+            .setDescription(dMsg.content);
+
+        msg.guild.me.client.deletedMessages.clear();
+        return msg.channel.send(embed);
+    }
+}
