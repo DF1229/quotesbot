4dc97fbb23fbf71cb400e4a10becdbc02876f985
DF1229 <daanfaber1@gmail.com>
2021-01-18 21:38:03 +0100

    Update shitpost.js

--- commands/shitpost.js
+++ commands/shitpost.js
@@ -4,15 +4,13 @@ const fs = require('fs');
 module.exports = {
     name: 'shitpost',
     description: 'Get a random shitpost the pile',
-    execute(msg) {
+    async execute(msg) {
         let embed = new Discord.MessageEmbed()
-            .setColor('GOLD')
-            .setFooter(msg.client.user.tag, msg.client.user.avatarURL({'dynamic': true}));
         
         const shitposts = fs.readdirSync('shitposts');
         const num = Math.floor(Math.random() * shitposts.length);
-        embed.setImage(`shitposts/${num}.jpg`);
-        embed.setTitle(`Shitpost #${num}`);
-        msg.channel.send(embed);
+        embed.attachFiles(`shitposts/${num}.jpg`);
+        const sMsg = await msg.channel.send(embed);
+        sMsg.suppressEmbeds();
     }    
 }
