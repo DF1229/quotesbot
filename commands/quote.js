80c9c8127206448aa984099cc787ffe94b35931e
DF1229 <daanfaber1@gmail.com>
2021-02-26 18:25:52 +0100

    Update quote.js
    Work to create a quotes commandhandler. This command would be combining all commands relating to quotes into a single commandhandler (and file).

--- commands/quote.js
+++ commands/quote.js
@@ -1,3 +1,4 @@
+const quoteHandler = require('../utils/quoteHandler.js');
 const { superusers } = require('../config.json');
 const Discord = require('discord.js');
 const fs = require("fs");
@@ -7,10 +8,10 @@ module.exports = {
     description: 'Handle all settings surrounding quotes',
     guildOnly: true,
     args: true,
-    usage: '<option> <value>',
+    usage: '<option> <setting> [value]',
     options: ['channel', 'tracking', 'add'],
     permissionLeveL: 'MANAGE_GUILD',
-    execute(msg, args, guildSettings) {
+    async execute(msg, args, guildSettings) {
         if (!this.options.includes(args[0])) {
             const iterator = this.options.values();
             let optionsValuesString = "";
@@ -28,21 +29,63 @@ module.exports = {
 
         switch(args[0]) {
             case 'channel':
-                
+                const chnlSettings = ['view', 'remove', 'set'];
+                if (!chnlSettings.includes(args[1])) {
+                    let embed = new Discord.MessageEmbed()
+                        .setColor('RED')
+                        .setTitle('Invalid setting!')
+                        .setDescription(`This option does not have that setting, try one of these: \`view\` \`remove\` \`set\`.`);
+                    return msg.channel.send(embed);
+                }
+
+                const newSettingsA = channel(args[1], args[2], guildSettings);
+                saveSettings(newSettingsA);
                 break;
             case 'tracking':
+                const trackSettings = ['disable', 'enable'];
+                if (!trackSettings.includes(args[1])) {
+                    let embed = new Discord.MessageEmbed()
+                        .setColor('RED')
+                        .setTitle('Invalid setting!')
+                        .setDescription(`This option does not have that setting, try one of these: \`enable\` \`disable\`.`);
+                    return msg.channel.send(embed);
+                }
 
+                const newSettingsB = tracking(args[1], args[2], guildSettings);
+                saveSettings(newSettingsB);
                 break;
             case 'add':
+                let quoteChnl = msg.guild.channels.cache.get(guildSettings.quotesChannel.id);
+                if (!quoteChnl) return msg.channel.send(`❌ Oops, I couldn't find this server's quotes channel! Is quote tracking enabled in this server?`);
+                 console.log(`quoteChnl:\n${quoteChnl}\n`);
+                let quoteMsg = await quoteChnl.messages.fetch(args[2]);
+                if (!quoteMsg) return msg.channel.send(`❌ Oops, I couldn't fetch the message from ${quoteChnl}! Check to see if you provided the right message ID!`);
+                 console.log(`quoteMsg:\n${quoteMsg}\n`);
+                // check to see if the bot already reacted to the message, which means it has already been handled
+                let qtMsgReactionManager = quoteMsg.reactions;
+                console.log(`qtMsgReactionManager:\n${qtMsgReactionManager}`);
 
+                //quoteHandler.run(quoteMsg);
                 break;
             default:
                 let errEmbed = new Discord.MessageEmbed()
                     .setColor('RED')
                     .setTitle('Invalid argument!')
-                    .setDescription(`ERR: VARIABLE VALUE NOT IN RANGE AT \`{this.args[0]}\`.`);
-                return msg.channel.send(errEmbed);
+                    .setDescription(`ERR: VARIABLE VALUE NOT IN RANGE AT \`{this.args[0]}\` (switch.default).`);
+                msg.channel.send(errEmbed);
                 break;
         }
     }
+}
+
+function channel(setting, value, guildSettings) {
+
+}
+
+function tracking(setting, value, guildSettings) {
+
+}
+
+function saveSettings(newSettings) {
+
 }
